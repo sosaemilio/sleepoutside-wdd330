5aa39ec4027e27cb2440c17da56bc720b4a4b59a
Emilio Sosa <emiliossosa85@gmail.com>
2023-04-30 22:40:41 -0400

    fixed cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,10 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const htmlItems = cartItemTemplate(cartItems);
+  //const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  //document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector(".product-list").innerHTML = htmlItems;
 }
 
 function cartItemTemplate(item) {
@@ -25,4 +27,4 @@ function cartItemTemplate(item) {
   return newItem;
 }
 
-renderCartContents();
+renderCartContents(); 
