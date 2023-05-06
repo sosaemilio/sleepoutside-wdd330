84082833aff31b0c7127158d815a460379365dd0
Emilio Sosa <emiliossosa85@gmail.com>
2023-05-05 21:01:14 -0400

    Moved functions to productDetails

--- src/js/product.js
+++ src/js/product.js
@@ -1,27 +1,6 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import { productDetails } from "./productDetails.mjs"
 
 let productID = getParam("product");
-findProductById(productID);
-
-
-function addProductToCart(product) {
-  let products = getLocalStorage("so-cart");
-  if (!Array.isArray(products)) { 
-    products = [products];
-    products.shift();
-  }
-  products.push(product);
-  setLocalStorage("so-cart", products);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+productDetails(productID);
 
