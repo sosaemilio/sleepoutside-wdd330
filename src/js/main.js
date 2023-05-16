8db9ebb4571858122a260f6d2d8ade793a315bf0
reiquel <carelary92@gmail.com>
2023-05-16 15:01:11 -0400

    new

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,3 @@ import productList from "./productList.mjs";
 productList("tents", "ul.product-list");
 
 
-const cartItems = document.querySelectorAll(".cart-item");
-
-for (const cartItem of cartItems) {
-  const x = document.createElement("span");
-  x.textContent = "X";
-  x.addEventListener("click", () => {
-    const id = cartItem.dataset.id;
-    const cart = localStorage.getItem("cart");
-    const newCart = JSON.parse(cart) || [];
-    newCart = newCart.filter(item => item.id !== id);
-    localStorage.setItem("cart", JSON.stringify(newCart));
-    cartItem.remove();
-  });
-  cartItem.appendChild(x);
-}
