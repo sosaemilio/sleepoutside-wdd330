fef6daf3e2648bef9f45de388d433aa60c9d37b6
reiquel <carelary92@gmail.com>
2023-05-16 14:37:27 -0400

    Remove items 1

--- src/js/main.js
+++ src/js/main.js
@@ -3,50 +3,18 @@ import productList from "./productList.mjs";
 productList("tents", "ul.product-list");
 
 
-// main.js
+const cartItems = document.querySelectorAll(".cart-item");
 
-// Attach click event listeners to the remove-product elements
-const removeProductButtons = document.querySelectorAll('.remove-product');
-removeProductButtons.forEach(button => {
-  button.addEventListener('click', handleRemoveProduct);
-});
-
-// Event handler for removing a product
-function handleRemoveProduct(event) {
-  const productId = event.target.dataset.id; // Get the product ID from the clicked X icon
-  const cart = JSON.parse(localStorage.getItem('cart')); // Retrieve cart data from localStorage
-
-  // Find the index of the product to remove in the cart array
-  const productIndex = cart.findIndex(item => item.id === productId);
-
-  if (productIndex !== -1) {
-    // Remove the product from the cart array
-    cart.splice(productIndex, 1);
-
-    // Update the cart in localStorage
-    localStorage.setItem('cart', JSON.stringify(cart));
-
-    // Re-render the cart list
-    renderCart();
-  }
-}
-
-// Function to render the cart list
-function renderCart() {
-  const cartList = document.querySelector('.cart-list');
-  const cart = JSON.parse(localStorage.getItem('cart')); // Retrieve cart data from localStorage
-
-  // Clear the existing cart list
-  cartList.innerHTML = '';
-
-  // Iterate over the cart items and create HTML for each item
-  cart.forEach(item => {
-    const cartItem = document.createElement('li');
-    cartItem.textContent = item.name;
-
-    cartList.appendChild(cartItem);
+for (const cartItem of cartItems) {
+  const x = document.createElement("span");
+  x.textContent = "X";
+  x.addEventListener("click", () => {
+    const id = cartItem.dataset.id;
+    const cart = localStorage.getItem("cart");
+    const newCart = JSON.parse(cart) || [];
+    newCart = newCart.filter(item => item.id !== id);
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    cartItem.remove();
   });
+  cartItem.appendChild(x);
 }
-
-// Initial rendering of the cart list
-renderCart();
