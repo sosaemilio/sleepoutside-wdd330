5ee000409979f7318aa39b8a9ea7eb80c5b54bdc
reiquel <carelary92@gmail.com>
2023-05-17 14:16:15 -0400

    new

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,35 +1,59 @@
-class Alert {
+export default class Alert {
     constructor() {
       this.alerts = [];
     }
   
-    readAlerts() {
-      const alertsJson = require('./alerts.json');
-      this.alerts = alertsJson.map((alert) => {
-        return {
-          message: alert.message,
-          background: alert.background,
-          color: alert.color,
-        };
-      });
+    async loadAlerts() {
+      try {
+        const response = await fetch("/json/alerts.json");
+        if (response.status !== 200) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        this.alerts = data;
+      } catch (error) {
+        console.error("Error loading alerts:", error);
+      }
     }
   
-    renderAlerts() {
-      const alertListSection = document.createElement('section');
-      alertListSection.className = 'alert-list';
+    createAlertElement(alert) {
+      const alertElement = document.createElement("p");
+      alertElement.textContent = alert.message;
+      alertElement.style.backgroundColor = alert.background;
+      alertElement.style.color = alert.color;
+      return alertElement;
+    }
+  
+    createAlertSection() {
+      const alertSection = document.createElement("section");
+      alertSection.classList.add("alert-list");
   
       this.alerts.forEach((alert) => {
-        const alertP = document.createElement('p');
-        alertP.style.backgroundColor = alert.background;
-        alertP.style.color = alert.color;
-        alertP.textContent = alert.message;
-  
-        alertListSection.appendChild(alertP);
+        const alertElement = this.createAlertElement(alert);
+        alertSection.appendChild(alertElement);
       });
   
-      document.querySelector('main').prepend(alertListSection);
+      return alertSection;
+    }
+  
+    prependAlertsToMain() {
+      const mainElement = document.querySelector(".alert-list");
+      if (mainElement) {
+        mainElement.prepend(this.createAlertSection());
+      } else {
+        console.warn('Main element with class .alert-list not found');
+      }
+    }
+  
+    async init() {
+      await this.loadAlerts();
+      if (this.alerts.length > 0) {
+        this.prependAlertsToMain();
+      }
     }
   }
   
-  export default Alert;
+  
+  
+  
   
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import productList from "./productList.mjs";
+import Alert from "./Alert.js";
 
 productList("tents", "ul.product-list");
+
+const alert = new Alert();
+alert.init();
 
 
