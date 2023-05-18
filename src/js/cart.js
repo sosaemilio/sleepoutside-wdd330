8df6f2417a686c48835376797699ef6c49a96662
reiquel <carelary92@gmail.com>
2023-05-18 11:20:27 -0400

    corrections

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,6 @@
-import { renderCartContents } from "./shoppingCart.mjs";
+import shoppingCart from "./shoppingCart.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
-renderCartContents();
+shoppingCart();
 loadHeaderFooter();
 
 
--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -1,11 +1,9 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
 
-export function renderCartContents() {
+export default function ShoppingCart() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-
-
+  const outputEl = document.querySelector(".product-list");
+  renderListWithTemplate(cartItemTemplate, outputEl, cartItems);
 }
 
  function cartItemTemplate(item) {
@@ -22,7 +20,6 @@ export function renderCartContents() {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-  <span data-id="${item.id}" class="remove-item">X</span>
 </li>`;
 
   return newItem;
