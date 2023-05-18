201f18142544bdc106718282b81ad73d5d88b1c3
reiquel <carelary92@gmail.com>
2023-05-17 21:40:59 -0400

    New modules

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,47 +1,6 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-
-  // Attaching event listener after rendering the cart items
-  document.querySelectorAll(".remove-item").forEach((item) => {
-    item.addEventListener("Click", removeItemFromCart);
-  });
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-  <span data-id="${item.id}" class="remove-item">X</span>
-</li>`;
-
-  return newItem;
-}
-
-function removeItemFromCart(event) {
-  const itemId = event.target.getAttribute("data-id");
-
-  let cartItems = getLocalStorage("so-cart");
-  cartItems = cartItems.filter((item) => item.id !== itemId); // Removing the clicked item
-
-  // Updating the cart items in local storage
-  setLocalStorage("so-cart", cartItems);
-
-  // Re-rendering the cart items
-  renderCartContents();
-}
-
+import { renderCartContents } from "./shoppingCart.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 renderCartContents();
+loadHeaderFooter();
+
+
--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,5 @@
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
-
+import { loadHeaderFooter } from "./utils.mjs";
 let productID = getParam("product");
 productDetails(productID);
