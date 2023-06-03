da020845010bdd01069304f5d962e80607c46b06
Jack Trevor <sbrobools@gmail.com>
2023-06-02 22:53:31 -0300

    Checkout page

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,5 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
-
+const baseURL = import.meta.env.VITE_SERVER_URL;
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -8,14 +7,25 @@ function convertToJson(res) {
   }
 }
 
-export function getProductsByCategory(category = "tents") {
-  return fetch(baseURL + `products/search/${category}`)
-    .then(convertToJson)
-    .then((data) => data.Result);
+export async function getProductsByCategory(category) {
+  const response = await fetch(baseURL + `products/search/${category}`);
+  const data = await convertToJson(response);
+  return data.Result;
 }
 
 export async function findProductById(id) {
-  return fetch(baseURL + `product/${id}`)
-    .then(convertToJson)
-    .then((data) => data.Result);
+  const response = await fetch(baseURL + `product/${id}`);
+  const product = await convertToJson(response);
+  return product.Result;
+}
+
+export async function checkout(payload) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+  return await fetch(baseURL + "checkout/", options).then(convertToJson);
 }
